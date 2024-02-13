7066091dd11e42c6398de847ad42a8b085dd502a
ILLezex <fengyulin777@gmail.com>
2024-02-13 10:19:17 -0500

    change the build folder to out and set unoptimized=true for image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: "build",
-    // output: "export"
+    images: {
+        unoptimized: true
+    },
+    // distDir: "build",
+    output: "export"
 }
 
 module.exports = nextConfig
